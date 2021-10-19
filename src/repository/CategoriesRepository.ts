5d56244a35ae16e4dfbee26da7cff75f4de06c46
Vinícius Barbosa Campelo <vinibcampelo@gmail.com>
2021-10-19 11:16:28 -0300

    Utilizando o princípio da substituição de liskov

---
+++ src/repository/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { Category } from "../model/category";
+import { ICategoriesRepository } from "./ICategoriesRepository";
+
+class CategoriesRepository implements ICategoriesRepository {
+    private categories: Category[];
+
+    constructor() {
+        this.categories = [];
+    }
+
+    create({ name, description}): void {
+        const category: Category = new Category();
+
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date()
+        });
+    
+        this.categories.push(category);
+    }
+
+    list(): Category[] {
+        return this.categories;
+    }
+
+    findByName(name: string): Category {
+        const category = this.categories.find(category => category.name === name);
+        return category;
+    }
+}
+
+export { CategoriesRepository }
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,24 @@
+import { CategoriesRepository } from "../repository/CategoriesRepository";
+import { ICategoriesRepository } from "../repository/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    execute({name, description}: IRequest): void {
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+        if(categoryAlreadyExists) {
+            throw new Error("Category Already exists!");
+        }
+    
+        this.categoriesRepository.create({name, description});
+    }
+}
+
+export { CreateCategoryService }
