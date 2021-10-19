3d8b06c9be386f92a81d934b6f254f56ea30f15d
Vinícius Barbosa Campelo <vinibcampelo@gmail.com>
2021-10-19 19:46:48 -0300

    Corrigindo as importações

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../repository/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repository/CategoriesRepository"; 
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService"; 
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
